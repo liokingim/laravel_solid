09560ed882010326967b4ce6105d9090dca6bd8b
liokingim <liokingim@gmail.com>
2023-10-30 17:49:22 +0900

    5: Dependency inversion principle (DIP)

--- app/Solid/StripePaymentMethod.php
+++ app/Solid/StripePaymentMethod.php
@@ -2,6 +2,12 @@
 
 namespace App\Solid;
 
+/**
+ * 5: Dependency inversion principle (DIP)
+ *
+ * 상위 모듈은 하위 모듈에 의존해서는 안된다. 상위 모듈과 하위 모듈 모두 추상화에 의존해야 한다.
+ * 추상화는 세부 사항에 의존해서는 안된다. 세부사항이 추상화에 의존해야 한다.
+ */
 class StripePaymentMethod implements PaymentMethodInterface
 {
 
